edc0b3276ddabca5975fd320670c5f6e18c8cb61
Ryan Brooks <rmb6616@psu.edu>
2026-02-15 17:55:50 -0500

    switched to gemma3:1b for better responses

--- lab04/lab04.py
+++ lab04/lab04.py
@@ -7,6 +7,11 @@ from util.llm_utils import AgentTemplate
 
 # Add code here
 
+encounter_templates = {
+    'npc': ('lab04/lab04_npc.json', 'Gabe Newell (NPC)'),
+    'enemy': ('lab04/lab04_enemy.json', 'Andrew Wilson (Enemy)'),
+}
+
 # But before here.
 
 def run_console_chat(template_file, agent_name='Agent', **kwargs):
@@ -24,15 +29,18 @@ def run_console_chat(template_file, agent_name='Agent', **kwargs):
         try:
             response = chat.send(input('You: '))
             # Add code here to check which agent chat should be started
-
-
+            for key, (template, name) in encounter_templates.items():
+                if key in response.lower():
+                    print(f'\n--- Entering {name} encounter ---\n')
+                    run_console_chat(template_file=template, agent_name=name)
+                    print(f'\n--- {name} encounter ended ---\n')
+                    break
             # But before here.
         except StopIteration as e:
             break
 
 if __name__ ==  '__main__':
     # Add code here to start DM chat
-
-
+    encounters = ', '.join(name for _, (_, name) in encounter_templates.items())
+    run_console_chat(template_file='lab04/lab04_dm.json', agent_name='DM', encounters=encounters)
     # But before here.
-    pass
